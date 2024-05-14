4a9ddde9586edfb3e5c3547a4c073340e72d5885
Mariano Paz <paz.mariano@gmail.com>
2024-05-14 11:44:05 -0300

    linting

--- helpers.sh
+++ helpers.sh
@@ -1,21 +1,21 @@
 FINOPS_STAGES=( preview-environments sandbox dev stage prod)
 
 find_overrides_with_content () {
-	find . -name 'override.tf' -type f | while read -r file
-	do
-		if grep -q 'provider "aws"' "$file"
-		then
-			echo $file
-		fi
-	done
+  find . -name 'override.tf' -type f | while read -r file
+  do
+    if grep -q 'provider "aws"' "$file"
+    then
+      echo $file
+    fi
+  done
 }
 
 find_by_env () {
-	for env in "${FINOPS_STAGES[@]}"
-	do
-		find . -name "$env.tfvars" -type f | while read -r file
-		do
-			echo $file
-		done
-	done
+  for env in "${FINOPS_STAGES[@]}"
+  do
+    find . -name "$env.tfvars" -type f | while read -r file
+    do
+      echo $file
+    done
+  done
 }
